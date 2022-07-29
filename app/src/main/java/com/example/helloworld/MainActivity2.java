35ad3fb890941baf90b0ed67ded6b38259b6f906
MD. Rakibul Hassan <rakibpranto123@gmail.com>
2022-07-29 15:39:45 +0600

    newCommit

--- app/src/main/java/com/example/helloworld/MainActivity2.java
+++ app/src/main/java/com/example/helloworld/MainActivity2.java
@@ -2,13 +2,28 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity2 extends AppCompatActivity {
-
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
+    private Button ld;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        ld = findViewById(R.id.button3);
+        ld.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.button3
+        ) {
+
+            Intent intent1 = new Intent(MainActivity2.this,MainActivity.class);
+            startActivity(intent1);
+        }
     }
 }
